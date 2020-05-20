a2cac37eb607e462e0d4157cecea9a726cc06c18
marten-mertan <mertan@mail.ru>
2020-05-20 15:32:43 +0300

    qritems mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,21 @@
-$('.js-qritems-slider').slick({
-    centerMode: true,
-    slidesToShow: 1,
-    arrows: true,
-    centerPadding: '0px',
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
+$(window).on('load', function(){
+    $('.js-qritems-slider').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        arrows: true,
+        centerPadding: '0px',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 1
+                }
             }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
+        ]
+    });
+    
+    $('.js-qritem-dropdown').on('click', function(event) {
+        $(this).toggleClass('active');
+    });
 });
