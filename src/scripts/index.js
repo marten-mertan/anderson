a499383a932a5c96dc19caf807662e964590c9e1
marten-mertan <mertan@mail.ru>
2020-09-09 06:25:23 +0300

    add video slider in qritems

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -80,6 +80,22 @@ $(window).on('load', function(){
             }
         ]
     });
+
+    $('.js-qritems-slider-video').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        arrows: true,
+        centerPadding: '0px',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
     
     $('.js-qritem-dropdown').on('click', function(event) {
         $(this).toggleClass('active');
