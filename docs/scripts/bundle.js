f398040c9399ef181f3ea00c82b559f4a81b3d14
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2020-12-15 22:18:21 +0300

    add fix order page

--- docs/scripts/bundle.js
+++ docs/scripts/bundle.js
@@ -383,6 +383,7 @@ $(window).on('load', function(){
             }
         }, delay));
     });
+
 });
 
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -293,4 +293,5 @@ $(window).on('load', function(){
             }
         }, delay));
     });
+
 });
